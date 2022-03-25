158403b11459ce2cdd62028b3dcc93b0de3e6412
Rohit <rohitnagar3112@gmail.com>
2022-03-25 13:36:15 +0530

    Commit by X Demo ist

---
+++ src/test/java/Academy/E2EProjectFramework/gitDemo.java
@@ -0,0 +1,13 @@
+package Academy.E2EProjectFramework;
+
+import org.testng.annotations.Test;
+
+public class gitDemo {
+
+	@Test
+	public void gitDemo()
+	{
+		System.out.println("Git demo add");
+		System.out.println("rohit nagar");
+	}
+}
